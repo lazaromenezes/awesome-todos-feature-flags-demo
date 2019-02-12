00217f22e7cdd766b75420ddf8c211d96ac8855a
Lazaro Menezes <lazaro.menezes@actminds.com>
2019-02-12 16:38:36 -0200

    Starts to store the todo-list into localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Todo } from './model/todo';
 import { UUID } from 'angular2-uuid';
+import { TodoRepository } from './repository/todo-repository'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'awesome-todo-list';
   currentTodo: string;
   todoList: Todo[];
 
-  constructor(){
+  constructor(private todoRepository: TodoRepository){
     
   }
 
+  ngOnInit(){
+
+    debugger;
+    this.todoList = this.todoRepository.load()
+  }
+
   addTodo(){
+    debugger;
     if(this.todoList === undefined)
       this.todoList = [];
 
@@ -25,6 +33,8 @@ export class AppComponent {
       title: this.currentTodo,
       isDone: false
     });
+
+    this.todoRepository.store(this.todoList);
 
     this.currentTodo = "";
   }
---
+++ src/app/repository/todo-repository.ts
@@ -0,0 +1,24 @@
+import {Todo} from '../model/todo'
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoRepository{
+    key: string = "todo-list";
+
+    load(): Todo[]{
+        let todoList = localStorage.getItem(this.key);
+
+        if(todoList === undefined || todoList === null)
+            return [];
+
+        return JSON.parse(todoList);
+    }
+
+    store(todoList: Todo[]){
+        let stringList = JSON.stringify(todoList);
+
+        localStorage.setItem(this.key, stringList);
+    }
+}
